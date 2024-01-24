7e219b6896457bbbb6efd65aa7b2579fd7aa0712
Ashish <243143.learn@gmail.com>
2024-01-24 18:57:39 +0530

    modification continue

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-/* =========== Scroll Selection Active Link =========== */
+/*=============== toggle icon navbar ===============*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+/*=============== scroll selection active link ===============*/
 let sections =document.querySelectorAll('section');
 let navLink =document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLink.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    /*=============== scroll selection active link ===============*/
+    let header = document.querySelector('header')
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*=============== remove toggle icon and navbar when click navbar link (scroll) ===============*/
     
-}
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+};
