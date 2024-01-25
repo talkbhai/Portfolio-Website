5997e53e03fe4f83dff93503f341836208697b26
Ashish <243143.learn@gmail.com>
2024-01-25 19:25:38 +0530

    some more modification

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-
 /*=============== scroll selection active link ===============*/
 let sections =document.querySelectorAll('section');
 let navLink =document.querySelectorAll('header nav a');
@@ -26,14 +25,36 @@ window.onscroll = () => {
             });
         };
     });
-    /*=============== scroll selection active link ===============*/
-    let header = document.querySelector('header')
+    /*=============== sticky navbar ===============*/
+    let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
     /*=============== remove toggle icon and navbar when click navbar link (scroll) ===============*/
-    
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
-
 };
+
+
+/*=============== scroll reveal ===============*/
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'righ' });
+
+
+/*=============== typed js ===============*/
+const typed = new typed ('.multiple-text', {
+    strings: ['Web Developer!', 'YouTuber', 'Content Creater'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
